155f5fce073d2552be3d0356e533a6cc99439ccb
IvaNaskk <82344430+IvaNaskk@users.noreply.github.com>
2022-09-04 18:43:54 +0200

    Update urls.py

--- proekt2/urls.py
+++ proekt2/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mealFix/', include('mealFix.urls')),
+    path('', include('mealFix.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
